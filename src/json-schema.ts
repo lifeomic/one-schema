1a1944979aecd6175c03b38c50ba7fd3de25e07c
Swain Molster <swain.molster@lifeomic.com>
2022-06-20 13:18:36 -0400

    fix: resolve refs in request schemas when validating

--- src/json-schema.ts
+++ src/json-schema.ts
@@ -44,3 +44,55 @@ export const transformJSONSchema = (
     }),
   );
 };
+
+/**
+ * "Resolves" any $ref entries within the provided `root` schema, using
+ * the provided `definitions`. Returns the "resolved" version of the schema.
+ *
+ * @example
+ * const schema = {
+ *   type: 'array',
+ *   items: {
+ *     $ref: '#/definitions/Item'
+ *   }
+ * }
+ *
+ * const resolved = withResolvedDefinitions(
+ *   schema,
+ *   {
+ *     Item: {
+ *       type: 'string',
+ *       description: 'An item'
+ *     }
+ *   }
+ * )
+ *
+ * console.log(resolved)
+ *
+ * // {
+ * //   type: 'array',
+ * //   items: {
+ * //     type: 'string',
+ * //     description: 'An item'
+ * //   }
+ * // }
+ */
+export const withResolvedDefinitions = (
+  root: JSONSchema4,
+  definitions: Record<string, JSONSchema4 | undefined>,
+): JSONSchema4 =>
+  transformJSONSchema(root, (schema) => {
+    if (!schema.$ref) {
+      return schema;
+    }
+
+    const resourceName = schema.$ref.split('/')[2];
+    if (!resourceName) {
+      throw new Error(`Encountered an invalid ref: ${schema.$ref}`);
+    }
+    const resource = definitions[resourceName];
+    if (!resource) {
+      throw new Error(`Encountered an invalid ref: ${schema.$ref}`);
+    }
+    return withResolvedDefinitions(resource, definitions);
+  });
