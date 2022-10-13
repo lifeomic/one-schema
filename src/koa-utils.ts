74290134f4b50d81d344d8f77ec4c1b3e59a0bba
Swain Molster <swain.molster@lifeomic.com>
2022-10-12 23:11:55 -0400

    refactor: pull out koa utilities into dedicated file

---
+++ src/koa-utils.ts
@@ -0,0 +1,168 @@
+import Router from '@koa/router';
+import { ExtendableContext } from 'koa';
+
+/**
+ * This type is structured recursively.
+ */
+export type PathParamsOf<Route> =
+  // First, filter out the leading method and space (e.g. the "GET ")
+  Route extends `${string} ${infer Path}`
+    ? PathParamsOf<Path>
+    : // Now, split by the "/", and check the strings before + after.
+    Route extends `${infer Before}/${infer After}`
+    ? PathParamsOf<Before> & PathParamsOf<After>
+    : // If the path part looks like a param, return the object.
+    Route extends `:${infer Param}`
+    ? {
+        [Key in Param]: string;
+      }
+    : {};
+
+/**
+ * We use this type to very cleanly remove these fields from the Koa context, so
+ * that we can replace the fields with our strict types from the generated schema.
+ *
+ * - `request.body`
+ * - `request.query`
+ *
+ * This is primarily important for `request.body` -- the others are included in the
+ * same why for simplicity.
+ *
+ * Why it's important for `request.body`: most popular Koa body parser middlewares use a
+ * global `declare` statement to mark the `body` field as being present on the request.
+ *
+ * Most of these libraries declare the field as `body: any`. With this type declaration,
+ * it becomes impossible to augment the type, since any more stringent types are just
+ * "collapsed" into the `any` type, resulting in a final type of `any`.
+ *
+ * By explicitly removing the fields from the context, then re-adding them, we can be
+ * sure they are typed correctly.
+ */
+export type ExtendableContextWithRequestFieldsRemoved =
+  // Omit request entirely
+  Omit<ExtendableContext, 'request'> & {
+    // Re-add request, but without the "body" or "query" fields.
+    request: Omit<ExtendableContext['request'], 'body' | 'query'>;
+  };
+
+export type StateOfRouter<RouterType> = RouterType extends Router<
+  infer State,
+  any
+>
+  ? State
+  : never;
+
+export type ContextOfRouter<RouterType> = RouterType extends Router<
+  any,
+  infer Context
+>
+  ? Context
+  : never;
+
+export type ContextOfEndpoint<
+  RouteName,
+  Request,
+  RouterType extends Router<any, any>,
+> =
+  // prettier-ignore
+  // 1. Start with a context that has request.body and request.query removed.
+  // This context also importantly does _not_ have the `params` property included,
+  // since it comes from a core `koa` type, rather than from `@koa/router`.
+  & ExtendableContextWithRequestFieldsRemoved
+  // 2. Now, we add the generated + well-typed `params`, `request.body`, and
+  // `request.query` properties. 
+  & {
+      params: PathParamsOf<RouteName>;
+      request: RouteName extends `${'GET' | 'DELETE'} ${string}`
+        ? { query: Request }
+        : { body: Request };
+    } 
+  // 3. Now, add the `state` property and merge in the arbitrary custom context, to
+  // essentially mimic the behavior of koa's `ParameterizedContext`. 
+  //
+  // Why not just use ParameterizedContext: When we tried to use ParameterizedContext
+  // directly, it was incompatible with Omit (omitting a single property resulted in
+  // a fully empty object).
+  & { state: StateOfRouter<RouterType> } & ContextOfRouter<RouterType>;
+
+export type EndpointImplementation<
+  RouteName,
+  Request,
+  Response,
+  RouterType extends Router<any, any>,
+> = (
+  context: ContextOfEndpoint<RouteName, Request, RouterType>,
+) => Response | Promise<Response>;
+
+export const implementRoute = <
+  Route extends string,
+  Request,
+  Response,
+  R extends Router<any, any>,
+>(
+  route: Route,
+  router: R,
+  parse: (ctx: ContextOfEndpoint<Route, Request, R>, data: unknown) => Request,
+  implementation: EndpointImplementation<Route, Request, Response, R>,
+) => {
+  // Separate method and path. e.g. 'POST my/route' => ['POST', 'my/route']
+  const [method, path] = route.split(' ');
+
+  /** A shared route handler. */
+  const handler: Router.Middleware<
+    StateOfRouter<R>,
+    ContextOfRouter<R>
+  > = async (ctx, next) => {
+    // 1. Validate the input data.
+    // 1a. For GET and DELETE, validate the query params.
+    if (['GET', 'DELETE'].includes(method)) {
+      ctx.request.query = parse(ctx, ctx.request.query);
+    } else {
+      // 1b. Otherwise, use the body.
+      ctx.request.body = parse(ctx, ctx.request.body);
+    }
+
+    // 2. Run the provided route handler.
+    const response = await implementation(ctx);
+
+    /**
+     * Why we avoid checking the response against the response schema:
+     *
+     * Current limitations of JSONSchema prevent us from doing this correctly,
+     * because of common inheritance-like approaches to defining response types.
+     * See this SO thread for a detailed description of the issue:
+     *
+     * https://stackoverflow.com/questions/22689900/json-schema-allof-with-additionalproperties
+     */
+
+    // 3. Return the result and call the next middleware.
+    ctx.response.body = response;
+    // If the response status is already set to a 200-level code, don't override it.
+    // This is the mechanism for allowing consumers to customize response codes.
+    if (ctx.response.status < 200 || ctx.response.status >= 300) {
+      ctx.response.status = 200;
+    }
+    return next();
+  };
+
+  // Register the route + handler on the router.
+  switch (method) {
+    case 'POST':
+      router.post(path, handler);
+      break;
+    case 'GET':
+      router.get(path, handler);
+      break;
+    case 'PUT':
+      router.put(path, handler);
+      break;
+    case 'PATCH':
+      router.patch(path, handler);
+      break;
+    case 'DELETE':
+      router.delete(path, handler);
+      break;
+    default:
+      throw new Error(`Unsupported method detected: ${route}`);
+  }
+};
--- src/koa.ts
+++ src/koa.ts
@@ -1,74 +1,25 @@
 import { JSONSchema4 } from 'json-schema';
-import type { ExtendableContext, ParameterizedContext } from 'koa';
+import type { ParameterizedContext } from 'koa';
 import type Router from '@koa/router';
 import type { EndpointsOf, IntrospectionResponse, OneSchema } from './types';
 import Ajv from 'ajv';
-
-/**
- * We use this type to very cleanly remove these fields from the Koa context, so
- * that we can replace the fields with our strict types from the generated schema.
- *
- * - `request.body`
- * - `request.query`
- *
- * This is primarily important for `request.body` -- the others are included in the
- * same why for simplicity.
- *
- * Why it's important for `request.body`: most popular Koa body parser middlewares use a
- * global `declare` statement to mark the `body` field as being present on the request.
- *
- * Most of these libraries declare the field as `body: any`. With this type declaration,
- * it becomes impossible to augment the type, since any more stringent types are just
- * "collapsed" into the `any` type, resulting in a final type of `any`.
- *
- * By explicitly removing the fields from the context, then re-adding them, we can be
- * sure they are typed correctly.
- */
-type ExtendableContextWithRequestFieldsRemoved =
-  // Omit request entirely
-  Omit<ExtendableContext, 'request'> & {
-    // Re-add request, but without the "body" or "query" fields.
-    request: Omit<ExtendableContext['request'], 'body' | 'query'>;
-  };
-
-type StateOfRouter<RouterType> = RouterType extends Router<infer State, any>
-  ? State
-  : never;
-
-type ContextOfRouter<RouterType> = RouterType extends Router<any, infer Context>
-  ? Context
-  : never;
+import {
+  ContextOfRouter,
+  EndpointImplementation,
+  implementRoute,
+  StateOfRouter,
+} from './koa-utils';
 
 export type ImplementationOf<
   Schema extends OneSchema<any>,
   RouterType extends Router<any, any>,
 > = {
-  [Name in keyof EndpointsOf<Schema>]: (
-    // prettier-ignore
-    context:
-      // 1. Start with a context that has request.body and request.query removed.
-      // This context also importantly does _not_ have the `params` property included,
-      // since it comes from a core `koa` type, rather than from `@koa/router`.
-      & ExtendableContextWithRequestFieldsRemoved
-      // 2. Now, we add the generated + well-typed `params`, `request.body`, and
-      // `request.query` properties.
-      & {
-          params: EndpointsOf<Schema>[Name]['PathParams'];
-          request: Name extends `${'GET' | 'DELETE'} ${string}`
-            ? { query: EndpointsOf<Schema>[Name]['Request'] }
-            : { body: EndpointsOf<Schema>[Name]['Request'] };
-        }
-      // 3. Now, add the `state` property and merge in the arbitrary custom context, to
-      // essentially mimic the behavior of koa's `ParameterizedContext`.
-      //
-      // Why not just use ParameterizedContext: When we tried to use ParameterizedContext
-      // directly, it was incompatible with Omit (omitting a single property resulted in
-      // a fully empty object).
-      & { state: StateOfRouter<RouterType>; }
-      & ContextOfRouter<RouterType>,
-  ) =>
-    | EndpointsOf<Schema>[Name]['Response']
-    | Promise<EndpointsOf<Schema>[Name]['Response']>;
+  [Name in keyof EndpointsOf<Schema>]: EndpointImplementation<
+    Name,
+    EndpointsOf<Schema>[Name]['Request'],
+    EndpointsOf<Schema>[Name]['Response'],
+    RouterType
+  >;
 };
 
 export type IntrospectionConfig = {
@@ -186,80 +137,27 @@ export const implementSchema = <
 
   // Iterate through every handler, and add a route for it based on
   // the key/route description.
-  for (const [endpoint, routeHandler] of Object.entries(implementation)) {
-    // Separate method and path. e.g. 'POST my/route' => ['POST', 'my/route']
-    const [method, path] = endpoint.split(' ');
+  for (const endpoint in implementation) {
+    const routeHandler = implementation[endpoint];
 
-    /** A shared route handler. */
-    const handler: Router.Middleware<
-      StateOfRouter<RouterType>,
-      ContextOfRouter<RouterType>
-    > = async (ctx, next) => {
-      // 1. Validate the input data.
-      const requestSchema = schema.Endpoints[endpoint].Request;
-      if (requestSchema) {
-        const parser: typeof parse = parse ?? defaultParse;
+    const parser: typeof parse = parse ?? defaultParse;
 
-        // 1a. For GET and DELETE, validate the query params.
-        if (['GET', 'DELETE'].includes(method)) {
-          // @ts-ignore
-          ctx.request.query = parser(ctx, {
-            endpoint,
-            schema: { ...requestSchema, definitions: schema.Resources },
-            data: ctx.request.query,
-          });
-        } else {
-          // 1b. Otherwise, use the body.
-          ctx.request.body = parser(ctx, {
-            endpoint,
-            schema: { ...requestSchema, definitions: schema.Resources },
-            data: ctx.request.body,
-          });
+    implementRoute(
+      endpoint,
+      router,
+      (ctx, data) => {
+        const requestSchema = schema.Endpoints[endpoint].Request;
+        /* istanbul ignore next */
+        if (!requestSchema) {
+          return data;
         }
-      }
-
-      // 2. Run the provided route handler.
-      const response = await routeHandler(ctx);
-
-      /**
-       * Why we avoid checking the response against the response schema:
-       *
-       * Current limitations of JSONSchema prevent us from doing this correctly,
-       * because of common inheritance-like approaches to defining response types.
-       * See this SO thread for a detailed description of the issue:
-       *
-       * https://stackoverflow.com/questions/22689900/json-schema-allof-with-additionalproperties
-       */
-
-      // 3. Return the result and call the next middleware.
-      ctx.response.body = response;
-      // If the response status is already set to a 200-level code, don't override it.
-      // This is the mechanism for allowing consumers to customize response codes.
-      if (ctx.response.status < 200 || ctx.response.status >= 300) {
-        ctx.response.status = 200;
-      }
-      return next();
-    };
-
-    // Register the route + handler on the router.
-    switch (method) {
-      case 'POST':
-        router.post(path, handler);
-        break;
-      case 'GET':
-        router.get(path, handler);
-        break;
-      case 'PUT':
-        router.put(path, handler);
-        break;
-      case 'PATCH':
-        router.patch(path, handler);
-        break;
-      case 'DELETE':
-        router.delete(path, handler);
-        break;
-      default:
-        throw new Error(`Unsupported method detected: ${endpoint}`);
-    }
+        return parser(ctx, {
+          endpoint,
+          schema: { ...requestSchema, definitions: schema.Resources },
+          data,
+        });
+      },
+      routeHandler,
+    );
   }
 };
